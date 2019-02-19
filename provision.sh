f4f19ae0d98ea062856299f1b0074df3128b868a
Tim Parker <Tim.ParkerD@gmail.com>
2019-02-19 00:26:06 -0600

    Python 2.7 successful installation

--- provision.sh
+++ provision.sh
@@ -1,7 +1,9 @@
-sudo yum update -y # update yum 
-sudo yum install -y centos-release-scl # install SCL
-sudo yum install -y python27 # install Python 2.7
-scl enable python27 "easy_install-2.7 pip"
-# scl enable python27 bash
-# cd /opt/rh/python27/root/usr/bin/ # cd to the directory where SCL installs python
-# sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ./easy_install-2.7
+#!/bin/bash
+sudo yum install -y gcc openssl-devel bzip2-devel
+sudo wget -P /home/vagrant https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
+sudo tar xzf /home/vagrant/Python-2.7.15.tgz
+cd /home/vagrant/Python-2.7.15/ && sudo ./configure --enable-optimizations
+cd /home/vagrant/Python-2.7.15 && sudo make altinstall
+sudo curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+export PATH="/usr/local/bin:$PATH"
+python2.7 get-pip.py
